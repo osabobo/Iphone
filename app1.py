5e4b38c9a30105f4ae4f98eb5edf0d652689a0f4
osagie eboigbe <osagie_eboigbe@yahoo.com>
2021-05-21 00:29:04 +0100

    file

--- app1.py
+++ app1.py
@@ -57,7 +57,7 @@ def main ():
 
     if add_selectbox == 'Online':
         Age = st.text_input('Age')
-        Salary = st.text_input('Salary(it is in dollar and the amount the client want to use to buy the phone,Whether monthly or yearly)')
+        Salary = st.text_input('Salary(USD/Annum)')
         Gender = st.selectbox('Gender', ["Male","Female"])
 
         output=""
