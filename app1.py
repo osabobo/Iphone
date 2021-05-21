91cd2a240bee4162988933c17a239a0a9c577921
osagie eboigbe <osagie_eboigbe@yahoo.com>
2021-05-21 08:38:03 +0100

    add one file

--- app1.py
+++ app1.py
@@ -3,37 +3,29 @@ import pandas as pd
 import joblib
 import os
 import streamlit as st
-
+from sklearn.preprocessing import LabelEncoder
 cv_model = open('predictions.pkl', 'rb')
 cv = joblib.load(cv_model)
 
-def prediction(Gender,Age,Salary):
+def predictions(Gender,Age,Salary):
     if Gender == "Male":
         Gender = 0
     else:
         Gender = 1
+
     Age=Age
+
     Salary=Salary
      # Making predictions
+
     prediction = cv.predict(
         [[Gender,Age,Salary]])
-
     if prediction == 0:
         pred = 'No'
     else:
         pred = 'Yes'
     return pred
-def predict1(data1):
 
-    vect =cv.texts_to_sequences(data1['v2'])
-    vect = pad_sequences(vect, maxlen=max_length, padding=padding_type, truncating=trunc_type)
-
-
-
-
-    predictions=model.predict(vect)
-
-    return predictions
 def main ():
     from PIL import Image
     image = Image.open('logo.jpg')
@@ -60,12 +52,13 @@ def main ():
         Salary = st.text_input('Salary(USD/Annum)')
         Gender = st.selectbox('Gender', ["Male","Female"])
 
-        output=""
+        result=""
+
 
 
 
         if st.button("Predict"):
-            result = prediction(Gender, Age,Salary)
+            result = predictions(Gender, Age,Salary)
             st.success(result)
 
 
@@ -75,23 +68,23 @@ def main ():
 
 
 
-
     if add_selectbox == 'Batch':
-
-
-        file_upload = st.file_uploader("Upload csv file for predictions", type=["csv"],encoding =None, key = 'a')
+        file_upload = st.file_uploader("Upload csv file for predictions", type="csv",encoding =None, key = 'a')
 
 
 
 
-        st.title('Make sure the csv File is in the same format  as spam.csv before uploading to avoid Error')
+        st.title('Make sure the csv File is in the same format  as iphone.csv before uploading to avoid Error')
 
         if file_upload is not None:
-            data1 = pd.read_csv(file_upload,encoding = 'latin-1')
+            data = pd.read_csv(file_upload)
+            data['Gender']= data['Gender'].map({'Male':0, 'Female':1})
+            data=data.drop('Purchase Iphone',axis=1)
+
+            predictions = cv.predict(data)
 
 
 
-            predictions =np.asarray(predict1(data1))
 
 
             st.write(predictions)
